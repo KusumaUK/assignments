dc2ee0acedd5d2e432394873c07e38b1c1c9f68e
KusumaUK <117565657+KusumaUK@users.noreply.github.com>
2022-11-11 23:38:00 +0000

    Initial commit

---
+++ src/main/java/week1/day1/NegativeToPositiveAssign.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class NegativeToPositiveAssign {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=-199;
+		if(n<0) {
+			System.out.println(-(n));
+		}
+		else {
+			System.out.println(n);
+		}
+	}
+
+}
